d54d9bb9c2b9dc6a1de393711146370da35a3d81
LucioBxll <lucio.boxall15@gmail.com>
2024-10-06 21:51:10 -0300

    Se le da un front

---
+++ public/materialice.js
@@ -0,0 +1,6 @@
+// MATERIALIZE CSS 
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, 0);
+  });
